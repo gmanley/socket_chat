fa5da11a03da4075c5cb24d312ed0a48df42e277
Gray Manley <gray.manley@gmail.com>
2011-11-16 15:19:29 -0600

    Make user email required.

--- lib/user.rb
+++ lib/user.rb
@@ -18,8 +18,8 @@ class User
 
   before_save :prepare_password
 
-  validates_presence_of :first_name, :last_name
-  validates_uniqueness_of :email, allow_blank: true
+  validates_presence_of :email, :first_name, :last_name
+  validates_uniqueness_of :email
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
   validate :check_password, on: :create
 
