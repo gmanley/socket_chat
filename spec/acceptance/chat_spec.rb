e40f5e9a1547e32404d330e54f43671aa925c57b
Gray Manley <gray.manley@gmail.com>
2011-11-23 15:52:21 -0600

    Tweak chat spec

--- spec/acceptance/chat_spec.rb
+++ spec/acceptance/chat_spec.rb
@@ -1,15 +1,17 @@
 require 'acceptance_helper'
 
-feature 'User can add messages' do
+feature 'Chat' do
   let(:user) { User.make! }
   let(:room) { Room.make! }
   let(:message) { Message.make }
 
   background do
-    sign_in(user.email, user.password)
+    sign_in(user.email, 'password')
   end
 
-  scenario "in a chat room" do
+  scenario "Posting a message" do
+    sign_in(user.email, 'password')
+
     visit "/room/#{room.slug}"
 
     within("#sendMessage") do
